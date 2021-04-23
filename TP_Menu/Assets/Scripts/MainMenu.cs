877a8134b9abcad70ba365f2f811aba2f0ca68d2
Raphael Bauvin <45790209+Rafyb@users.noreply.github.com>
2021-04-23 16:23:16 +0200

    Update

---
+++ TP_Menu/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image ImageFade;
+    public List<MainMenuButton> Buttons;
+
+    public void OnCLickPlay()
+    {
+        ImageFade.DOFade(1, 0.8f).OnComplete(FadeComplete);
+
+        for(int i = 0; i < Buttons.Count; i++)
+        {
+            if (i == 0)
+            {
+                Buttons[i].Hide(1f);
+            }
+            else
+            {
+                Buttons[i].Hide(0.1f);
+            }
+        }
+        
+    }
+
+    private void FadeComplete()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+}
---
+++ TP_Menu/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,48 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public Image ImageOutline;
+    public TMPro.TextMeshProUGUI Text;
+    public Color ColorInitial;
+    public Color ColorSelected;
+
+    private bool _hidden = false;
+
+    public void OnPointerEnter()
+    {
+        if (_hidden) return;
+
+        ImageOutline.DOKill();
+        ImageOutline.DOFade(1,0.2f);
+
+        Text.DOKill();
+        Text.DOColor(ColorSelected, 0.2f);
+    }
+
+    public void OnPointerExit()
+    {
+        if (_hidden) return;
+
+        ImageOutline.DOKill();
+        ImageOutline.DOFade(0, 0.1f);
+
+        Text.DOKill();
+        Text.DOColor(ColorInitial, 0.2f);
+    }
+
+    public void Hide(float time)
+    {
+        _hidden = true;
+
+        ImageOutline.DOKill();
+        ImageOutline.DOFade(0,time);
+
+        Text.DOKill();
+        Text.DOFade(0, time);
+    }
+}
